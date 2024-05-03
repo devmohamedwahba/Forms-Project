476fda3516fcc4a106a9dc04cc541d13613b12e3
mohaedwahba <m.wahba@pointseven>
2024-05-03 17:44:15 +0300

    adding white noise to work static files

--- forms_django/settings.py
+++ forms_django/settings.py
@@ -185,8 +185,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STORAGES = {
-    'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
-    }
-}
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
